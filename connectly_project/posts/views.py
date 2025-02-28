7bdb5e62ce3812bfee89593d690848676791c5b1
hyuxjinboi <hyuxjinboi@gmail.com>
2025-02-28 14:49:25 +0800

    changes

--- connectly_project/posts/views.py
+++ connectly_project/posts/views.py
@@ -8,21 +8,6 @@ from rest_framework.permissions import AllowAny
 
 
 # User List & Create API
-class UserListCreate(APIView):
-    permission_classes = [AllowAny]
-
-    def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 # Post List, Create, Update & Delete API
 class PostListCreate(APIView):
@@ -32,13 +17,6 @@ class PostListCreate(APIView):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
-
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PostDetail(APIView):
